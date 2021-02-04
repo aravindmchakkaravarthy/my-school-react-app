c51dcd61db71b879ee3791e1594d80022ef19aa2
Aravind Mangadu Chakkaravarthy <aravind@aravinds-mbp.lan>
2021-02-04 18:50:31 -0500

    Added functionality for game play

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,144 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
 import { Header } from './components/header/header';
 import { Footer } from './components/footer/footer';
+import spellingBee from './spelling-bee.png';
 
+import {Button} from 'react-bootstrap';
 
+class App extends Component {
 
-function App() {
-  return (
-    <>
+  state = {
+    playStarted: false,
+    playComplete: false,
+    currentScore: 0,
+    total: 0,
+    word: {
+      word: '',
+      id: '',
+      url: '',
+      spelling: ''
+    },
+    backgroundColor: ''
+  }
+
+  componentDidMount(){
+
+  }
+
+  startPlay =  () => {
+    this.setState({playStarted: true, playComplete:false});
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ currentScore: this.state.currentScore, total:this.state.total })
+  };
+  let url = 'http://myschoolapp-env.eba-3ujvyqcp.us-east-1.elasticbeanstalk.com/play';
+  //let url = 'http://localhost:5000/play';
+  fetch(url, requestOptions)
+      .then(response => response.json())
+      .then(data => this.setState({
+        currentScore:data.currentScore, 
+        total:data.total,
+        word:{
+          word: data.nextWord.word,
+          id: data.nextWord.id,
+          url: data.nextWord.url,
+          spelling: ''
+        }
+      }));
+  }
+
+  nextPlay = () => {
+    let url = 'http://localhost:5000/play';
+    console.log(this.state.word);
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ 
+        currentScore: this.state.currentScore, 
+        total:this.state.total,
+        currentWord:{
+          word: this.state.word.word,
+          spelling: this.state.word.spelling,
+        } 
+      })
+  };
+    fetch(url, requestOptions)
+        .then(response => response.json())
+        .then(data => {
+          this.setState({
+          currentScore:data.currentScore, 
+          total:data.total,
+          word:{
+            word: data.nextWord.word,
+            id: data.nextWord.id,
+            url: data.nextWord.url,
+            spelling: ''
+          }
+        })
+        if(data.result){
+          this.setState({backgroundColor: 'lightgreen'});
+        }else{
+          console.log("setting background color");
+          this.setState({backgroundColor: '#ff4b4b'});
+        }
+        setTimeout(()=>this.setState({backgroundColor: ''}), 200);
+      });
+        
+  }
+
+  
+
+  closePlay = () => {
+    this.setState({playComplete:true})
+  }
+
+  changeSpelling = (evt:any) => {
+    let word = this.state.word;
+    word.spelling = evt.target.value;
+    this.setState({word:word})
+  }
+
+  render() {
+    const {playStarted, word, currentScore, total, playComplete} = this.state;
+    return (
+      <>
       <Header />
-      
-      <div>
-        <audio src="https://ssl.gstatic.com/dictionary/static/sounds/oxford/define--_us_1.mp3" controls autoPlay />
+      <div className="body" style={{backgroundColor:this.state.backgroundColor}}>
+        {playComplete? <>
+          <div style={{fontSize:'2rem', margin:'20px'}}>
+            <span>Your score</span>
+            <div className="end-score"><div style={{margin:'auto'}}>{currentScore}/{total}</div></div>
+          
+          </div>
+        </>:<>
+          {playStarted ? 
+          <div style={{margin:'10px' }}>
+            <div className="score"><div style={{margin:'auto'}}>{currentScore}/{total}</div></div>
+            <audio style={{margin:'20px'}}src={word.url} autoPlay controls></audio>
+            <input type="text" className="form-control" value={word.spelling} onChange={this.changeSpelling}/>
+          </div>: <img src={spellingBee} className="welcome-image"  />}
+        </>}
       </div>
-      <div className="card">
-        <div className="card-body">
-          <h5 className="card-title">Steve Jobs</h5>
-          <h6 className="card-subtitle mb-2 text-muted">steve@apple.com</h6>
-          <p className="card-text">Stay Hungry, Stay Foolish</p>
+      <div className="footer">
+        <div className="navbar navbar-dark bg-dark box-shadow">
+          <div className="d-flex justify-content-between">
+          {playComplete? <><Button variant="success" style={{margin:'20px'}} onClick={this.startPlay}>Play again</Button></>:
+          <>
+            {!playStarted ? <Button variant="success" style={{margin:'20px'}} onClick={this.startPlay}>Play</Button> : 
+            <>
+            <Button variant="success" style={{margin:'20px'}} onClick={this.nextPlay}>Next</Button>
+            <Button variant="secondary" style={{margin:'20px'}} onClick={this.closePlay}>End Game</Button>
+            </>}</>}
+          </div>
         </div>
       </div>
-      <div className="card">
-        <div className="card-body">
-          <h5 className="card-title">Steve Jobs</h5>
-          <h6 className="card-subtitle mb-2 text-muted">steve@apple.com</h6>
-          <p className="card-text">Stay Hungry, Stay Foolish</p>
-        </div>
-      </div>
-      <Footer />
     </>
-  );
+    );
+  }
 }
+
 
 export default App;
---
+++ src/components/header/header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const Header = () => {
+ return (
+  <header>
+   <div className="navbar navbar-dark bg-dark box-shadow">
+    <div className="d-flex justify-content-between">
+     <a href="#" className="navbar-brand d-flex">
+      <strong>Spelling Bee</strong>
+     </a>
+    </div>
+   </div>
+  </header>
+ );
+};
